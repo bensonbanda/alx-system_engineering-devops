c227e83b481202d6e263e4545fe55f8de97025ac
bensonbanda <bbanda79@yahoo.com>
2023-02-03 06:21:21 -0800

    add files

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -2,5 +2,5 @@
 #
 from = ARGV[0].scan(/from:(.*?)\]/)
 to = ARGV[0].scan(/to:(.*?)\]/)
-flags = ARGV[0].scan(/flags:(.*?)\])
+flags = ARGV[0].scan(/flags:(.*?)\]/)
 puts [from, to, flags].join(',')
